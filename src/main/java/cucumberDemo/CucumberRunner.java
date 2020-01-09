f66bc9888e54e5fd6fad75c5929c8be4f74a7ed3
Stanislav Makarkin <Stanislav_Makarkin@epam.com>
2020-01-09 15:05:54 +0400

    New tests8

--- src/main/java/cucumberDemo/CucumberRunner.java
+++
@@ -1,9 +0,0 @@
-package cucumberDemo;
-
-import cucumber.api.CucumberOptions;
-import cucumber.api.testng.AbstractTestNGCucumberTests;
-
-@CucumberOptions(features = "src/main/resources/features", glue={"src/main/cucumberDemo/"}, tags = "@all")
-public class CucumberRunner extends AbstractTestNGCucumberTests {
-
-}
