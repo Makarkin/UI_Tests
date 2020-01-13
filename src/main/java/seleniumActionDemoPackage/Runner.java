dfa2be5471e802436c8c2663b5c61e201bfe09f8
Stanislav Makarkin <Stanislav_Makarkin@epam.com>
2020-01-13 17:48:46 +0400

    New tests8

--- src/main/java/seleniumActionDemoPackage/Runner.java
+++ src/main/java/seleniumActionDemoPackage/Runner.java
@@ -8,6 +8,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+@SuppressWarnings("deprecation")
 public class Runner {
     public static void main(String[] args) {
         TestNG testNG = new TestNG(false);
