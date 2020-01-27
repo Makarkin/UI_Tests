15c0b12f0fe0e319171aa73578e8973073ae7cf3
Stanislav Makarkin <Stanislav_Makarkin@epam.com>
2020-01-27 19:26:03 +0400

    Updating page classes

--- RunTest.bat
+++ RunTest.bat
@@ -1,4 +1,1 @@
-set classpath=C:\Users\Stanislav_Makarkin\.m2\repository\org\testng\testng\6.14.3\testng-6.14.3.jar;C:\Users\Stanislav_Makarkin\Gradle\caches\modules-2\files-2.1\com.beust\jcommander\1.78\a3927de9bd6f351429bcf763712c9890629d8f51\jcommander-1.78.jar;C:\Users\Stanislav_Makarkin\Repositories\UI_Tests\target\allure-demo-1.0-SNAPSHOT.jar
-cd C:\Users\Stanislav_Makarkin\IdeaProjects\allure-demo
-java org.testng.TestNG src/main/resources/suites/tests.xml
-pause
+mvn clean test
